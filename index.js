41e445fed53beed81e8803adcffb298e51cf277e
khnhk0ogei04 <khanh8a04@gmail.com>
2024-06-10 01:45:54 +0700

    Cài đặt pug

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "pug");
 app.get("/", (req, res) => {
-    res.send("Trang Chủ");
+    res.render("clients/pages/home/index.pug");
 });
 
 app.get("/products", (req, res) => {
-    res.send("Trang Danh Sách Sản Phẩm")
+    res.render("clients/pages/products/index.pug")
 });
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
