42f1cce8cd5e9c1a32424348af5dfab68a795761
khnhk0ogei04 <khanh8a04@gmail.com>
2024-06-10 02:11:59 +0700

    Cấu hình folder Router

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
+
+const routeClient = require("./routes/index.routes");
 const app = express();
 const port = 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
-app.get("/", (req, res) => {
-    res.render("clients/pages/home/index.pug");
-});
-
-app.get("/products", (req, res) => {
-    res.render("clients/pages/products/index.pug")
-});
+routeClient.index(app);
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
